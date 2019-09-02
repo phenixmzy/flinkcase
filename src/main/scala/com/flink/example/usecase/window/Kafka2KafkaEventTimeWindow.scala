79808803c4fb30926a0cce580543e7c3ad49b9ce
mazhiyong <wingmazhiyong@gmail.com>
2019-09-02 12:13:07 +0800

    Case EventTime Window

--- src/main/scala/com/flink/example/usecase/window/Kafka2KafkaEventTimeWindow.scala
+++ src/main/scala/com/flink/example/usecase/window/Kafka2KafkaEventTimeWindow.scala
@@ -1,5 +1,6 @@
 package com.flink.example.usecase.window
 
+import java.text.SimpleDateFormat
 import java.util.concurrent.TimeUnit.SECONDS
 
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
@@ -8,12 +9,13 @@ import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.{CheckpointingMode, TimeCharacteristic}
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer011, FlinkKafkaProducer011}
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks
 import org.apache.flink.streaming.api.windowing.time.Time
 import com.alibaba.fastjson.JSON
 import com.flink.example.usecase.ParamsAndPropertiesUtil
+import org.apache.flink.streaming.api.watermark.Watermark
 
-
-object Kafka2KafkaWindow {
+object Kafka2KafkaEventTimeWindow {
   val ONE_SECONDS = 1000L
   val ONE_MIN = 60 * ONE_SECONDS
   val CHECK_POINT_TIMEOUT = 10 * ONE_MIN
@@ -23,6 +25,11 @@ object Kafka2KafkaWindow {
 
     // set up the streaming execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    /**
+      * 这个case使用事件时间.
+      * */
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
     env.getConfig.disableSysoutLogging()
     env.getConfig.setRestartStrategy(RestartStrategies.fixedDelayRestart(4, 10000))
@@ -55,7 +62,6 @@ object Kafka2KafkaWindow {
       return
     }
 
-
     val inputTopic = params.getRequired("input-topic")
     val outputTopic = params.getRequired("output-topic")
     val isNoKey = params.getRequired("is-nokey")
@@ -73,10 +79,30 @@ object Kafka2KafkaWindow {
       val json = JSON.parseObject(jsonContent)
       val gameId = json.getString("game_id")
       val startTime = json.getIntValue("start_time")
-      val leave_time = json.getIntValue("leave_time")
-      val timeLen = leave_time - startTime
-      (gameId, timeLen)
-    })
+      val leaveTime = json.getIntValue("leave_time")
+      val timeLen = leaveTime - startTime
+      (gameId, timeLen, startTime)
+    }).assignTimestampsAndWatermarks(
+      /**
+        * watermarks的生成方式有两种
+        * 1：With Periodic Watermarks：周期性的触发watermark的生成和发送
+        * 2：With Punctuated Watermarks：基于某些事件触发watermark的生成和发送
+        * */
+      new AssignerWithPeriodicWatermarks[(String, Int, Int)]() {
+      var currentMaxtTimestamp: Long = 0L
+      private val maxOutOfOrderness = 3500L
+      val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+
+      override def getCurrentWatermark: Watermark = new Watermark(currentMaxtTimestamp - maxOutOfOrderness)
+
+      override def extractTimestamp(t: (String, Int, Int), l: Long): Long = {
+        val timeStamp = t._3 * 1000
+        currentMaxtTimestamp = Math.max(timeStamp.toLong, currentMaxtTimestamp)
+        System.out.println("key:"+t._1+", eventtime:["+t._3+"|"+sdf.format(t._3 * 1000)+"], currentMaxTimestamp:["+currentMaxtTimestamp+"|"+
+          sdf.format(currentMaxtTimestamp )+"], watermark:["+getCurrentWatermark().getTimestamp() + "|"+sdf.format(getCurrentWatermark().getTimestamp())+"]");
+        timeStamp
+      }
+    }).map(item => (item._1, item._2))
 
     if (isNoKey.equals("nokey")) {
       gamePlayStream.timeWindowAll(Time.minutes(5), Time.minutes(5))
@@ -90,7 +116,6 @@ object Kafka2KafkaWindow {
         .map(item => "keyBy-"+item.toString())
         .addSink(kafkaProducer)
     }
-
     env.execute()
   }
 }
