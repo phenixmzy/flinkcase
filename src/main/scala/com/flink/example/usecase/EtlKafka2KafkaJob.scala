2e6faeec58ca1665b35b37a422670c19d3a5f8cb
mazhiyong <wingmazhiyong@gmail.com>
2019-09-16 17:33:54 +0800

    update case

--- src/main/scala/com/flink/example/usecase/EtlKafka2KafkaJob.scala
+++ src/main/scala/com/flink/example/usecase/EtlKafka2KafkaJob.scala
@@ -3,9 +3,6 @@ package com.flink.example.usecase
 import java.beans.Transient
 
 import com.alibaba.fastjson.JSON
-import com.flink.example.usecase.CaseUtil.GamePlay
-import com.flink.example.usecase.TestRandom.{delayRand, playTimeLenRand}
-import com.flink.example.usecase.source.GamePlaySource
 import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.api.java.utils.ParameterTool
@@ -13,10 +10,10 @@ import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.kafka.{FlinkKafkaConsumer011, FlinkKafkaProducer011}
 import org.apache.flink.streaming.api.scala.createTypeInformation
 import org.apache.flink.streaming.api.CheckpointingMode
-import org.apache.flink.streaming.api.functions.source.SourceFunction
-import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
 
 import scala.util.Random
+import scala.util.parsing.json.JSONObject
+
 
 object EtlKafka2KafkaJob {
   val ONE_SECONDS = 1000L
@@ -29,6 +26,7 @@ object EtlKafka2KafkaJob {
 
     // set up the streaming execution environment
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+
     env.getConfig.disableSysoutLogging()
     env.getConfig.setRestartStrategy(RestartStrategies.fixedDelayRestart(4, 10000))
 
@@ -42,12 +40,8 @@ object EtlKafka2KafkaJob {
     env.setParallelism(taskNum)
 
     //Controlling Latency
-    env.setBufferTimeout(100)
+    //env.setBufferTimeout(100)
     env
-  }
-
-  def main(args: Array[String]) {
-    executor(args)
   }
 
   def executor(args: Array[String]): Unit = {
@@ -60,11 +54,32 @@ object EtlKafka2KafkaJob {
       return
     }
     val env = setEvn(params)
+    val inputTopic = params.getRequired("input-topic")
     val outputTopic = params.getRequired("output-topic")
+    val kafkaConsumer = new FlinkKafkaConsumer011(inputTopic, new SimpleStringSchema, params.getProperties)
     val kafkaProducer = new FlinkKafkaProducer011(outputTopic, new SimpleStringSchema, params.getProperties)
 
-    val sourceStream = env.addSource(new GamePlaySource()).uid("create-data-gameplay")
-    sourceStream.map(item => item.toString()).uid("gameplay-data-to-string").addSink(kafkaProducer)
+    val sourceStream = env.addSource(kafkaConsumer).uid("consumer-gameplay-data-from-kafka")
+    sourceStream.map(jsonStr => {
+      val delayRand = new Random()
+      val playTimeLenRand = new Random()
+      val currTimeStamp = System.currentTimeMillis()/1000
+      val delay = delayRand.nextInt(300)
+      val timeLen = playTimeLenRand.nextInt(300)
+
+      val obj = JSON.parseObject(jsonStr)
+      val gameSleep = obj.getInteger("sleep")
+      val leaveTime = currTimeStamp - delay
+      val startTime = leaveTime - timeLen - gameSleep
+      val channel = obj.getString("from")
+      obj.put("start_time", startTime)
+      obj.put("leave_time", leaveTime)
+      obj.put("game_exp", timeLen)
+      obj.put("from_channel", channel)
+      obj.remove("from")
+
+      obj.toJSONString
+    }).uid("create-game-play-data").addSink(kafkaProducer)
 
     // execute program
     env.execute("DataCollect-GamePlay")
