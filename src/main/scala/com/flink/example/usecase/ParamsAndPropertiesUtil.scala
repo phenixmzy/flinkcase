6f7306517356e1a7c0b6c02768e3102744b22825
mazhiyong <wingmazhiyong@gmail.com>
2019-10-14 18:54:28 +0800

    Update

--- src/main/scala/com/flink/example/usecase/ParamsAndPropertiesUtil.scala
+++ src/main/scala/com/flink/example/usecase/ParamsAndPropertiesUtil.scala
@@ -1,6 +1,8 @@
 package com.flink.example.usecase
 
 //import io.confluent.kafka.serializers.KafkaAvroSerializer
+import java.util.Properties
+
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.kafka.common.serialization.StringSerializer
 import org.apache.kafka.clients.consumer.ConsumerConfig
@@ -8,31 +10,28 @@ import org.apache.kafka.clients.producer.ProducerConfig
 
 object ParamsAndPropertiesUtil {
   def loadKafkaCommonProperties(params: ParameterTool) = {
+    val kafkaProperties = new Properties()
     // config consumer
-    params.getProperties.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG,"524288000")
-    params.getProperties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"60000")
-    params.getProperties.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG,"100000")
-    params.getProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, params.getRequired("bootstrap.servers"))
-//    params.getProperties.put(ConsumerConfig.GROUP_ID_CONFIG, params.get("group.id"))
+    kafkaProperties.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG,"524288000")
+    kafkaProperties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,"60000")
+    kafkaProperties.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG,"100000")
+    kafkaProperties.put(ConsumerConfig.GROUP_ID_CONFIG, params.getRequired("group.id"))
 
     // config producer
-    params.getProperties.put(ProducerConfig.BATCH_SIZE_CONFIG,"200000")
-    params.getProperties.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "60000")
-    params.getProperties.put(ProducerConfig.LINGER_MS_CONFIG, "100")
-    //params.getProperties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "lz4")
+    kafkaProperties.put(ProducerConfig.BATCH_SIZE_CONFIG,"200000")
+    kafkaProperties.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "60000")
+    kafkaProperties.put(ProducerConfig.LINGER_MS_CONFIG, "100")
+    kafkaProperties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "lz4")
+    kafkaProperties.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG,"524288000")
+    kafkaProperties.put(ProducerConfig.MAX_BLOCK_MS_CONFIG,"134217728")
+    kafkaProperties.put(ProducerConfig.SEND_BUFFER_CONFIG,"134217728")
+    kafkaProperties.put(ProducerConfig.BUFFER_MEMORY_CONFIG,"134217728")
 
-    params.getProperties.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG,"524288000")
-    params.getProperties.put(ProducerConfig.MAX_BLOCK_MS_CONFIG,"134217728")
-    params.getProperties.put(ProducerConfig.SEND_BUFFER_CONFIG,"134217728")
-    params.getProperties.put(ProducerConfig.BUFFER_MEMORY_CONFIG,"134217728")
+    kafkaProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
+    kafkaProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
 
-    params.getProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-    params.getProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer])
-    //params.getProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[KafkaAvroSerializer])
-    //params.getProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[KafkaAvroSerializer])
+    kafkaProperties.put("bootstrap.servers", params.getRequired("bootstrap.servers"))
 
-    params.getProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, params.getRequired("bootstrap.servers"))
-
-    params.getProperties
+    kafkaProperties
   }
 }
